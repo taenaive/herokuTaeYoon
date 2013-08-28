d1bdd767936135a0925b1736625c2f6488f677c2
taenaive <taenaive@gmail.com>
2013-08-28 11:58:18 -0400

    response header set crash bug fix

--- app.js
+++ app.js
@@ -50,11 +50,14 @@ function checkAuth(req, res, next) {
 }
 //enables CORS for express.js
 app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  
   //heroku
   if( (process.env.PORT !== undefined) && (req.headers['x-forwarded-proto']!='https') ){
     res.redirect('https://'+req.host+req.url);
+  }
+  else{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
   }
   next();
  });
